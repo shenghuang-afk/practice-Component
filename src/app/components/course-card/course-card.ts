9af0848ab128b04a00114911ed1fdab07a13fc6c
Sheng Wei Huang <shengweihuang@dyn184-208.wireless-1725.ndsu.NoDak.edu>
2026-02-25 11:47:55 -0600

    created course card

---
+++ src/app/components/course-card/course-card.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-course-card',
+  imports: [],
+  templateUrl: './course-card.html',
+  styleUrl: './course-card.css',
+})
+export class CourseCard {
+
+}
